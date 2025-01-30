a407a880191907a12d27a227a94fb38d00b4a5c3
Pegoretti <rafaelpegorettipegoretti@gmail.com>
2025-01-30 20:44:05 -0300

    correção para salvar os registros no banco de maneira correta

--- monitoring/models.py
+++ monitoring/models.py
@@ -5,7 +5,7 @@ class MonitoringData(models.Model):
     mac = models.CharField(max_length=50)
     date = models.DateTimeField()
     object_class = models.CharField(max_length=100)
-    evidence_path = models.CharField(max_length=255, null=True, blank=True)
+    evidence_path = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"{self.mac} - {self.object_class}"
+        return f"{self.mac} - {self.object_class} - {self.date}"
